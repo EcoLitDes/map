31ff7e530b4a00d2633ddfde574431118c3f2c09
EcoLitDes <137975600+EcoLitDes@users.noreply.github.com>
2024-01-27 22:07:02 +0800

    Update points.js

--- points.js
+++ points.js
@@ -1,4 +1,3 @@
-javascript
 var pointJson = {
   "type": "FeatureCollection",
   "features": [
@@ -10,45 +9,16 @@ var pointJson = {
       },
       "properties": {
         "name": "Hong Kong Wetland Park",
-        "url": "https://ecodesignliteracy.hku.hk/hong-kong-wetland-park",
-        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-HKWP.jpg?raw=true"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [113.9640511, 22.3043674, 0]
+        "description": "<a href=\"http://hku.hk\" target=\"_parent\">Read more</a>",
+        "styleUrl": "#icon-1899-0288D1",
+        "icon-opacity": 1,
+        "icon-color": "#0288d1",
+        "icon-scale": 1,
+        "icon-offset": [32, 64],
+        "icon-offset-units": ["pixels", "insetPixels"],
+        "icon": "https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png",
+        "media": "https://raw.githubusercontent.com/iuansonleung/CEP/main/Downsampled/ThumbnailsHong%20Kong%20Wetland%20Park-C.jpg"
       },
-      "properties": {
-        "name": "Tung Chung New Town Extension (East) Eco-shoreline",
-        "url": "https://ecodesignliteracy.hku.hk/tung-chung-ecoshoreline",
-        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-TCE.jpg?raw=true"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [114.115245, 22.494667, 0]
-      },
-      "properties": {
-        "name": "Fanling Golf Course",
-        "url": "https://ecodesignliteracy.hku.hk/fanling-golf-course",
-        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-FL.jpg?raw=true"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [114.051804, 22.451433, 0]
-      },
-      "properties": {
-        "name": "Sha Po Marsh in Park Yoho",
-        "url": "https://ecodesignliteracy.hku.hk/park-yoho",
-        "media": "https://github.com/EcoLitDes/map/blob/0e081c3770865a359cc82d4233c9bdb128382b7a/Thumbnail/Thumbnail-YH.jpg?raw=true"
-      }
     }
   ]
 };
